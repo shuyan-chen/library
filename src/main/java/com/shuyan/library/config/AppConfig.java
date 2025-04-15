e1011ae5f82e4fb7fc5597c96ab557107347747d
shuyan-chen <shuyan.chen@innowise.com>
2025-04-15 15:55:11 +0200

    updated

--- src/main/java/com/shuyan/library/config/AppConfig.java
+++ src/main/java/com/shuyan/library/config/AppConfig.java
@@ -3,7 +3,6 @@ package com.shuyan.library.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 @ComponentScan(basePackages = "com.shuyan.library")
 public class AppConfig {
