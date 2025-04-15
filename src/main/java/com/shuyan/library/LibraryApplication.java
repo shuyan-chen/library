a21220f168eed40451d78a9d68ff5c83bee5c389
shuyan-chen <shuyan.chen@innowise.com>
2025-04-15 15:26:25 +0200

    updated

--- src/main/java/com/shuyan/library/LibraryApplication.java
+++ src/main/java/com/shuyan/library/LibraryApplication.java
@@ -1,21 +1,22 @@
 package com.shuyan.library;
 
+import com.shuyan.library.config.AppConfig;
 import com.shuyan.library.ui.Console;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan("com.shuyan.library")
+
 public class LibraryApplication {
+
+
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext(LibraryApplication.class)) {
-            System.out.println("=== Library Management System ===");
-            var ui = context.getBean(Console.class);
-            ui.run();
-        } catch (Exception e) {
-            System.err.println("Application failed to start: " + e.getMessage());
-            System.exit(1);
-        }
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Console console = context.getBean(Console.class);
+        console.run();
+        context.close();
     }
+
 }
---
+++ src/main/java/com/shuyan/library/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.shuyan.library.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackages = "com.shuyan.library")
+public class AppConfig {
+
+}
